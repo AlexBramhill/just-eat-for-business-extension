fd8866b2da9f6590704599352dc1b382f2027dcd
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2026-03-06 01:02:30 +0000

    limit manifest to just justeat

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,1 +1,22 @@
-console.log("Add content");
+import {logger} from "../shared/logger";
+import {features} from "./features/features.ts";
+
+const processPage = () => {
+    features.forEach(feature => {
+        if (feature.shouldRun()) {
+            feature.run();
+        }
+    });
+};
+
+const observer = new MutationObserver(() => {
+    logger.debug("DOM mutated, processing page again");
+    processPage();
+});
+
+observer.observe(document, {
+    childList: true,
+    subtree: true,
+});
+
+processPage();
