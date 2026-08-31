b4c4503a41c7088b950f5f3383f6d00166ea01f9
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2026-08-31 17:31:48 +0100

    add eslint

---
+++ eslint.config.ts
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import {defineConfig} from "eslint/config";
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        ignores: ["dist/**"],
+        plugins: {js},
+        extends: ["js/recommended"],
+        languageOptions: {globals: globals.browser},
+        rules: {
+            "no-restricted-imports": ["error", "import1", "import2"],
+        }
+    },
+    tseslint.configs.recommended,
+]);
