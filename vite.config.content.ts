d27d01efe72f9f01b709c61c8d7acb7aadbe9851
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2026-09-01 01:18:39 +0100

    fix build

--- vite.config.content.ts
+++ vite.config.content.ts
@@ -1,23 +1,24 @@
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import {resolve} from "path";
+import {defineConfig} from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ mode }) => ({
-  plugins: [tailwindcss()],
-  build: {
-    outDir: "dist/content",
-    emptyOutDir: mode !== "development",
-    lib: {
-      entry: resolve(__dirname, "src/content/index.ts"),
-      name: "contentScript",
-      formats: ["iife"],
-      fileName: () => "index.js",
+export default defineConfig(({mode}) => ({
+    plugins: [tsconfigPaths(), tailwindcss()],
+    build: {
+        outDir: "dist/content",
+        emptyOutDir: mode !== "development",
+        lib: {
+            entry: resolve(__dirname, "src/content/index.ts"),
+            name: "contentScript",
+            formats: ["iife"],
+            fileName: () => "index.js",
+        },
+        rollupOptions: {
+            output: {
+                assetFileNames: "assets/[name].[ext]",
+            },
+        },
     },
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name].[ext]",
-      },
-    },
-  },
-  publicDir: false,
+    publicDir: false,
 }));
