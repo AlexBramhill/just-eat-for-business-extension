302db0ffe10f1e16d9722c33c287761755a00577
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2026-09-01 01:47:59 +0100

    remove dead code

--- src/popup/signals/booleanToggleStore.ts
+++
@@ -1,9 +0,0 @@
-import { createSignal } from 'solid-js';
-
-export const createBooleanToggle = (initialValue: boolean = false) => {
-  const [value, setValue] = createSignal(initialValue);
-
-  const toggleValue = () => setValue((v) => !v);
-
-  return [value, toggleValue] as const;
-};
